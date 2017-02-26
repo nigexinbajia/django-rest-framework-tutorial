77e8da038811efda502651aa23e4c7eef2d5176b
anshengme <ianshengme@gmail.com>
2017-02-26 22:11:18 +0800

    使用通用类视图 只允许更改

--- apps/snippets/views.py
+++ apps/snippets/views.py
@@ -1,38 +1,15 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, \
-    DestroyModelMixin
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import ListAPIView, RetrieveDestroyAPIView
 
 
 # Create your views here.
 
-class SnippetListView(ListModelMixin, CreateModelMixin, GenericAPIView):
-    """
-    展示或创建snippets.
-    """
+class SnippetListView(ListAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetailView(RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView):
-    """
-    修改或删除一个snippet
-    """
+class SnippetDetailView(RetrieveDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
