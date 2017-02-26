386e0e8ec2b55a5ca074b32a881f4d7e36307568
anshengme <ianshengme@gmail.com>
2017-02-26 21:51:05 +0800

    第二章

--- apps/snippets/urls.py
+++ apps/snippets/urls.py
@@ -5,8 +5,11 @@
 
 from django.conf.urls import url
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^$', views.snippet_list),  # 获取snippet列表
     url(r'^(?P<pk>[0-9]+)/$', views.snippet_detail),  # 差删改snippets
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- apps/snippets/views.py
+++ apps/snippets/views.py
@@ -1,28 +1,18 @@
-from django.shortcuts import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
 # Create your views here.
 
-class JSONResponse(HttpResponse):
-    """
-    用于返回JSON数据
-    """
-
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)  # 序列化
-        kwargs['content_type'] = 'application/json'  # 返回类型
-        super(JSONResponse, self).__init__(content, **kwargs)
 
 
-@csrf_exempt
-def snippet_list(request):
+@api_view(['GET', 'POST'])
+def snippet_list(request, format=None):
     """
-    展示所有snippets，或创建新的snippet
+    展示或创建snippets.
     """
     if request.method == 'GET':
         """
@@ -30,21 +20,20 @@ def snippet_list(request):
         """
         snippets = Snippet.objects.all()
         serializers = SnippetSerializer(snippets, many=True)  # 显示多条
-        return JSONResponse(serializers.data)
+        return Response(serializers.data)
     elif request.method == 'POST':
         """
         如果是POST，获取请求头里面的数据，交给serializers验证，如果格式验证成功就保存并发数据返回到前端，否则返回格式错误的信息给前端
         """
-        data = JSONParser().parse(request)
-        serializers = SnippetSerializer(data=data)
+        serializers = SnippetSerializer(data=request.data)
         if serializers.is_valid():
             serializers.save()
-            return JSONResponse(serializers.data, status=201)
-        return JSONResponse(serializers.errors, status=400)
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def snippet_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk, format=None):
     """
     修改或删除一个snippet
     """
@@ -54,27 +43,26 @@ def snippet_detail(request, pk):
         """
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         """
         如果是GET方法，通过SnippetSerializer获取到代码片段并以JSON数据返回给前端
         """
         serializer = SnippetSerializer(snippet)
-        return JSONResponse(serializer.data)
-    elif request.method == 'POST':
+        return Response(serializer.data)
+    elif request.method == 'PUT':
         """
-        如果是POST请求，则获取请求头里面的数据，然后交给SnippetSerializer进行验证， 如果格式正确就更新，否则就返回错误信息
+        如果是PUT请求，则获取请求头里面的数据，然后交给SnippetSerializer进行验证， 如果格式正确就更新，否则就返回错误信息
         """
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(snippet, data=data)
+        serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
     elif request.method == 'DELETE':
         """
         如果是DELETE则直接删除代码段
         """
         snippet.delete()
-        return HttpResponse(status=204)
+        return Response(status=204)
