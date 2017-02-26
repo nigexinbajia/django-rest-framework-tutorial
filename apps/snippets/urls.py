f6744d8f6cef0d0458718a7791c12580617135b2
anshengme <ianshengme@gmail.com>
2017-02-26 22:03:50 +0800

    使用类视图重写API

--- apps/snippets/urls.py
+++ apps/snippets/urls.py
@@ -8,8 +8,8 @@ from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'^$', views.snippet_list),  # 获取snippet列表
-    url(r'^(?P<pk>[0-9]+)/$', views.snippet_detail),  # 差删改snippets
+    url(r'^$', views.SnippetListView.as_view()),  # 获取snippet列表
+    url(r'^(?P<pk>[0-9]+)/$', views.SnippetDetailView.as_view()),  # 差删改snippets
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- apps/snippets/views.py
+++ apps/snippets/views.py
@@ -1,27 +1,27 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 # Create your views here.
 
-
-
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
+class SnippetListView(APIView):
     """
     展示或创建snippets.
     """
-    if request.method == 'GET':
+
+    def get(self, request, format=None):
         """
         如果是GET请求就获取所有代码段，并通过JSONResponse返回给前端
         """
         snippets = Snippet.objects.all()
         serializers = SnippetSerializer(snippets, many=True)  # 显示多条
         return Response(serializers.data)
-    elif request.method == 'POST':
+
+    def post(self, request, format=None):
         """
         如果是POST，获取请求头里面的数据，交给serializers验证，如果格式验证成功就保存并发数据返回到前端，否则返回格式错误的信息给前端
         """
@@ -32,37 +32,43 @@ def snippet_list(request, format=None):
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None):
+class SnippetDetailView(APIView):
     """
     修改或删除一个snippet
     """
-    try:
-        """
-        获取snippet数据，如果获取不到就返回404
-        """
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+    def get_object(self, pk):
+        try:
+            """
+            获取snippet数据，如果获取不到就返回404
+            """
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
         """
-        如果是GET方法，通过SnippetSerializer获取到代码片段并以JSON数据返回给前端
-        """
+       如果是GET方法，通过SnippetSerializer获取到代码片段并以JSON数据返回给前端
+       """
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
-    elif request.method == 'PUT':
+
+    def put(self, request, pk, format=None):
         """
         如果是PUT请求，则获取请求头里面的数据，然后交给SnippetSerializer进行验证， 如果格式正确就更新，否则就返回错误信息
         """
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=400)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, pk, format=None):
         """
         如果是DELETE则直接删除代码段
         """
+        snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=204)
